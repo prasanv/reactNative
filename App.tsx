ffe6e8d2dd23473da6821f9d15b327131389f839
prasanv <bala.prasanv@gmail.com>
2023-12-09 14:44:54 -0800

    minor updates to pod file

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import {StyleSheet, Text, View} from 'react-native';
 function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Hello Prasan!</Text>
+      <Text>Hello Prasan! Welcome to React Native</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
   },
 });
