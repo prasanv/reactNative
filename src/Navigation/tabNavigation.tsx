1fe2db67a078b7bd30aeab2f357b9b6130a85590
prasanv <bala.prasanv@gmail.com>
2023-12-24 18:28:49 -0800

    tab navigator options

--- src/Navigation/tabNavigation.tsx
+++ src/Navigation/tabNavigation.tsx
@@ -1,15 +1,40 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Main from '../screens/Main';
 import Settings from '../screens/settings';
 import Articles from '../screens/articles';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Main" component={Main} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: true,
+        tabBarStyle: {position: 'absolute'},
+        tabBarItemStyle: {
+          borderColor: 'black',
+          borderWidth: 1,
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarActiveBackgroundColor: 'grey',
+        tabBarInactiveBackgroundColor: 'white',
+        tabBarLabelPosition: 'beside-icon',
+        tabBarBadgeStyle: {backgroundColor: 'lightblue'},
+      }}>
+      <Tab.Screen
+        name="Main"
+        component={Main}
+        options={{
+          title: 'Home',
+          tabBarBadge: 4,
+          tabBarIcon: ({color}) => (
+            <Ionicons name="home" size={32} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen name="Articles" component={Articles} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
